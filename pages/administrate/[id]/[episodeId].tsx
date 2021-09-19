33b8cc1dfac98fc69cc2ab53f47dc0a064c9f3ba
SeungGumon <b9327912@gmail.com>
2021-09-19 20:55:45 +0900

    episode upload done

--- pages/administrate/[id]/[episodeId].tsx
+++ pages/administrate/[id]/[episodeId].tsx
@@ -3,11 +3,18 @@ import {gql, useQuery, useReactiveVar} from "@apollo/client";
 import {isLoggedInVar} from "../../../apolloClient";
 import PleaseLogin from "../../../component/PleaseLogin";
 import NotAccept from "../../../component/NotAccept";
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Header} from "../../../component/Header";
 import {useRouter} from "next/router";
 import {adminFindByIdEpisode, adminFindByIdEpisodeVariables} from "../../../__generated__/adminFindByIdEpisode";
 import html2canvas from "@nidi/html2canvas";
+
+
+
+interface IImages {
+    src : string,
+    seq : number
+}
 
 
 const ADMIN_FIND_BY_ID_EPISODE = gql`
@@ -37,14 +44,39 @@ const EpisodeAdmin = () => {
             seriesId: Number(seriesId),
             episodeId: Number(episodeId)
         },
-        onCompleted: data => {
-            console.log(data.adminFindByIdEpisode);
-        }
+        // onCompleted: data => {
+        //     console.log(data.adminFindByIdEpisode);
+        // }
     })
 
 
 
-    const [imageList , setImageList] = useState<string[]>([]);
+    const [imageList , setImageList] = useState<IImages[]>([]);
+
+
+    const convertToFileAndUploadFile = async (base64String: string) => {
+        try {
+            const imageFile = await fetch(base64String)
+                .then(res => res.blob())
+                .then(blob => {
+                    const convertFile = new File([blob], "imageFile", {type: "image/png"});
+                    return convertFile
+                });
+
+            const formBody = new FormData();
+
+
+            formBody.append('file', imageFile);
+
+            const {image:url} = await (await fetch("http://localhost:5000/uploads/", {
+                method: "POST",
+                body: formBody
+            })).json();
+            return url;
+        } catch {
+            return alert("이미지를 올릴 수 없습니다");
+        }
+    }
 
 
     const captureToImage = async () => {
@@ -52,15 +84,52 @@ const EpisodeAdmin = () => {
         if (textarea) {
             textarea.style.width = '720px';
             textarea.style.height = '1097px';
-            await html2canvas(textarea).then((canvas) => {
+            await html2canvas(textarea).then(async (canvas) => {
                 const img = canvas.toDataURL();
-                setImageList((prev) => [...prev, img]);
+                textarea.style.width = '100%';
+                textarea.style.height = '187px';
+                const imgSrc = await convertToFileAndUploadFile(img);
+                setImageList((prev) => [...prev, {src: imgSrc, seq: imageList.length + 1}]);
             });
-            textarea.style.width = '100%';
-            textarea.style.height = '187px';
+
         }
     }
 
+
+
+    const deleteImage = (index : number) => {
+        const deleteImage = imageList.filter((image,filterIndex) => filterIndex !== index);
+        setImageList(deleteImage);
+    }
+
+
+    const changeSeq = (seq: number, imageSrc: string) => {
+        const newImageArr = imageList.map((image, mapIndex) => {
+            if (image.src === imageSrc) {
+                return {
+                    ...image,
+                    seq
+                }
+            } else {
+                return image
+            }
+        });
+        setImageList(newImageArr)
+    }
+
+
+    const sortImage = () => {
+        const newArr = imageList.sort((a,b) => {
+            if ( a.seq < b.seq ){
+                return -1;
+            }
+            if ( a.seq > b.seq ){
+                return 1;
+            }
+            return 0;
+        });
+        setImageList(() => newArr);
+    }
 
 
     if (!isLoggedIn) {
@@ -103,29 +172,51 @@ const EpisodeAdmin = () => {
                     <h2 className={'text-md lg:text-xl text-gray-600'}>{data.adminFindByIdEpisode?.series.name} {data.adminFindByIdEpisode.episode}화</h2>
 
                     <article className={'flex flex-col bg-white w-full text-gray-600'}>
-                        <h6 className={'py-3 '}>텍스트 편집기</h6>
-                        <textarea rows={16} cols={12} className={'border w-full p-3 whitespace-pre-wrap break-all'} style={{'fontSize':'25px','height':'187px','padding':'85px','lineHeight':'63px'}} id={'target'}/>
-                        <button className={"bg-yellow-300 hover:bg-yellow-400 py-3 mt-3 w-full rounded mx-auto"} onClick={() => captureToImage()}>저장하기</button>
+                        <h6 className={'py-3'}>텍스트 편집기</h6>
+                        <textarea rows={16} cols={12} className={'border w-full p-3 whitespace-pre-wrap break-all'}
+                                  style={{
+                                      'fontSize': '25px',
+                                      'height': '187px',
+                                      'padding': '85px',
+                                      'lineHeight': '63px'
+                                  }} id={'target'}/>
+                        <button className={"bg-yellow-300 hover:bg-yellow-400 py-3 my-3 w-full rounded mx-auto"}
+                                onClick={() => captureToImage()}>이미지 저장하기
+                        </button>
                     </article>
 
 
-                        <article className={'flex w-full flex-wrap items-center mx-10'}>
-                            {
-                                imageList.map((image,index) => {
-                                    return (
-                                        <div className={'w-1/6 border mx-1.5 mt-1.5'}>
-                                            <img key={index} src={image} style={{'width':'720px','height':'auto'}}/>
+                    <article className={'flex w-full flex-wrap items-center py-4'}>
+                        {
+                            imageList.map((image, index) => {
+                                return (
+                                    <React.Fragment key={index}>
+                                        <div className={'w-1/6 mx-1.5 mt-1.5 flex flex-col items-center'}>
+                                            <img className={'border '} src={image.src}
+                                                 style={{'width': '720px', 'height': 'auto'}}/>
+                                            <button
+                                                className={'bg-rose-500 hover:bg-rose-700 mt-1 py-1 text-xs w-4/6 mx-auto rounded text-white'}
+                                                onClick={() => deleteImage(index)}>
+                                                삭제
+                                            </button>
+                                            <input onChange={(e) => changeSeq(Number(e.target.value), image.src)}
+                                                   value={image.seq} placeholder={'이미지 순서'} type="number" min={0}
+                                                   className="pl-2 text-center text-md border rounded mt-1 placeholder-blue w-4/6 p-0 no-outline text-dusty-blue-darker"/>
                                         </div>
-                                    )
-                                })
-                            }
-                        </article>
 
+                                    </React.Fragment>
 
-
-
+                                )
+                            })
+                        }
+                    </article>
+                </section>
+                <section className={'w-full'}>
+                    <button className={'w-3/6 bg-blue-300 py-2 rounded hover:bg-blue-500'} onClick={() => sortImage()}>이미지 재배열 하기</button>
+                    <button className={'w-3/6 bg-lime-300 py-2 rounded hover:bg-lime-500'}>수정하기</button>
                 </section>
             </section>
+
         </>
     )
 }

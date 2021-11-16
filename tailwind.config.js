cfcd3a02f4f2555ab946cafc8ee28ba10b3968b6
SeungGumon <b9327912@gmail.com>
2021-11-17 00:12:48 +0900

    change package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.tsx','./component/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
